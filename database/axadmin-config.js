bc5b486c10070ac68c805426c82b89b87e14fdc3
colleencaveney <colleen.caveney@o-ms.com>
2016-06-23 10:52:03 -0400

    Express App

---
+++ database/axadmin-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    datasource: {
+        host: 'localhost',
+        user: 'across_xilinx',
+        port: '3320',
+        password: 'xxxxxxx',
+        database: 'across_xilinx',
+        connectionLimit: 5,
+    }
+};
