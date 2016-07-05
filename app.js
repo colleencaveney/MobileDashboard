dcc9ab990f82676996ceb8789c551e6ad3027608
colleencaveney <colleen.caveney@o-ms.com>
2016-07-05 14:55:59 -0400

    Express App

--- app.js
+++ app.js
@@ -5,28 +5,60 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var mysql = require('mysql');
-const config = require(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] + '/acrossConf/mobileDashboard.js');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var data = require('./service/db');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var mysql = require('mysql');
+const config = require(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] + '/acrossConf/mobileDashboard.js');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+init(startServer);
 
-app.use('/', routes);
-app.use('/users', users);
+// connect to database etc, dependencies before we can start the server
+function init(cb) {
+  // make sure we can connect to database
+  var connection = mysql.createConnection({
+    host : config.datasource.host,
+    user : config.datasource.user,
+    password : config.datasource.password,
+    port: config.datasource.port,
+    database: config.datasource.database,
+    connectionLimit: config.datasource.connectionLimit
+  });
+  var dbs = {};
+  connection.connect(function(err) {
+    if (err) return cb(err, dbs);
+    console.log("Database Connected");
+    dbs.connection = connection;
+    
+  });
+}
+
+function startServer(err, dbs) {
+  if (err) {
+    log.error("Initialization error:" + err);
+    if (dbs.connection) dbs.connection.end();
+    return;
+  }
+  var app = express();
+
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+
+  // uncomment after placing your favicon in /public
+  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.use('/', routes);
+  app.use('/data', data);
+
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +71,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get('env') === 'dev') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
