f3a5ccfaf477b800c05ad6679c8aec032647709c
colleencaveney <colleen.caveney@o-ms.com>
2016-07-06 11:08:28 -0400

    Express App

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var data = require('./service/db');
+var data = require('./service/licenseService');
 
 var app = express();
 
@@ -58,38 +58,41 @@ function startServer(err, dbs) {
   app.use('/', routes);
   app.use('/data', data);
 
-}
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // error handlers
 
-// error handlers
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'dev') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'dev') {
+  // production error handler
+  // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: {}
     });
   });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+  app.listen(config.server.port);
+  log.info("server started at " + config.server.port);
+
+}
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var licenseService = require("../service/licenseService");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dashboard' });
 });
+
+function resultHandler(res, next) {
+  return function (err, result) {
+    if (err) return next(err);
+
+    res.status(200).json(result);
+  }
+}
+
+router.get('/24HourLCount', function (req, res, next) {
+  licenseService.getLicenseCount24Hours(req.dbs, req.query.dataRange, resultHandler(res, next));
+});
+router.get('/top3AUsers', function (req, res, next) {
+  licenseService.getTop3AccountUsers(req.dbs, req.query.dataRange, resultHandler(res, next));
+});
+router.get('/top3EUsers', function (req, res, next) {
+  licenseService.getTop3EvalUsers(req.dbs, req.query.dataRange, resultHandler(res, next));
+});
+router.get('/AUserLCount', function (req, res, next) {
+  licenseService.getAccountUserLicenseCount(req.dbs, req.query.dataRange, resultHandler(res, next));
+});
+router.get('/EUserLCount', function (req, res, next) {
+  licenseService.getEvalUserLicenseCount(req.dbs, req.query.dataRange, resultHandler(res, next));
+});
+router.get('UAUserLCount', function (req, res, next) {
+  licenseService.getUniqueAccountUserLicenseCount(req.dbs, req.query.dataRange, resultHandler(res, next));
+});
+router.get('/UEUserLCount', function (req, res, next) {
+  licenseService.getUniqueEvalUserLicenseCount(req.dbs, req.query.dataRange, resultHandler(res, next));
+});
+
 
 module.exports = router;
